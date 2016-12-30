cb548f88f04e8d3532121c441b95914d6526a377
nuxsmin <nuxsmin@syspass.org>
2016-12-30 01:08:01 +0100

    Setup locales

--- sysPass-dev/entrypoint.sh
+++ sysPass-dev/entrypoint.sh
@@ -3,7 +3,29 @@
 XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-"172.17.0.1"}
 XDEBUG_IDE_KEY=${XDEBUG_IDE_KEY:-"ide"}
 
+setup_locales() {
+  if [ ! -e ".setup" ]; then
+    echo -e "\nSetting up locales ..."
+    sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' \
+    -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' \
+    -e 's/# ca_ES.UTF-8 UTF-8/ca_ES.UTF-8 UTF-8/' \
+    -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' \
+    -e 's/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' \ 
+    -e 's/# ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+
+    echo 'LANG="en_US.UTF-8"' > /etc/default/locale
+    dpkg-reconfigure --frontend=noninteractive locales
+    update-locale LANG=en_US.UTF-8
+
+    LANG=en_US.UTF-8
+    
+    echo "." > .setup
+ fi
+}
+
 trap "echo 'Stopping Apache2 ...' && /usr/sbin/apachectl stop" HUP INT QUIT KILL TERM
+
+setup_locales
 
 echo -e "Setting xdebug variables ...\n"
 sed -i 's/__XDEBUG_REMOTE_HOST__/'"$XDEBUG_REMOTE_HOST"'/' /etc/php5/apache2/conf.d/20-xdebug.ini
