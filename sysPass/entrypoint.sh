d8d127a043da1e02dc351f605cf9788f013abb9a
nuxsmin <nuxsmin@syspass.org>
2017-01-03 08:04:41 +0100

    Minor tweaks and locales setup

--- sysPass/entrypoint.sh
+++ sysPass/entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+setup_locales() {
+  if [ ! -e ".setup" ]; then
+    LOCALE_GEN="/etc/locale.gen"
+
+    echo -e "\nSetting up locales ..."
+
+    echo -e "\n### sysPass locales" >> $LOCALE_GEN
+    echo "es_ES.UTF-8 UTF-8" >> $LOCALE_GEN
+    echo "en_US.UTF-8 UTF-8" >> $LOCALE_GEN
+    echo "de_DE.UTF-8 UTF-8" >> $LOCALE_GEN
+    echo "ca_ES.UTF-8 UTF-8" >> $LOCALE_GEN
+    echo "fr_FR.UTF-8 UTF-8" >> $LOCALE_GEN
+    echo "ru_RU.UTF-8 UTF-8" >> $LOCALE_GEN
+
+    echo 'LANG="en_US.UTF-8"' > /etc/default/locale
+
+    dpkg-reconfigure --frontend=noninteractive locales
+    update-locale LANG=en_US.UTF-8
+
+    LANG=en_US.UTF-8
+
+    echo "1" > .setup
+ fi
+}
+
 trap "echo 'Stopping Apache2 ...' && /usr/sbin/apachectl stop" HUP INT QUIT KILL TERM
+
+setup_locales
 
 echo -e "Starting Apache2 ...\n"
 /usr/sbin/apache2ctl -D FOREGROUND &
 
 wait $!
-
-
-
