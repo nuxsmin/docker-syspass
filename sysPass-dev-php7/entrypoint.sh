d64116297f9e3e1d88e551320ac2660f1324acef
Rubén Domínguez <rdb@tier1.es>
2018-02-07 17:47:11 +0100

    Improved Docker file Using Debian Stretch as base image Using MariaDB as default database Using composer for installing dependencies

--- sysPass-dev-php7/entrypoint.sh
+++ sysPass-dev-php7/entrypoint.sh
@@ -3,6 +3,37 @@
 XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-"172.17.0.1"}
 XDEBUG_IDE_KEY=${XDEBUG_IDE_KEY:-"ide"}
 
+setup_app () {
+    [[ ! -d "./sysPass" ]] && mkdir sysPass
+
+    if [ ! -e "./sysPass/index.php" ]; then
+        echo -e "\nUnpacking sysPass ..."
+
+        unzip ${SYSPASS_BRANCH}.zip \
+        && mv sysPass-${SYSPASS_BRANCH}/* sysPass \
+        && chown ${APACHE_RUN_USER}:${SYSPASS_UID} -R sysPass/ \
+        && chmod g+w -R sysPass/ \
+        && chmod 750 sysPass/config sysPass/backup
+    fi
+}
+
+setup_composer () {
+    echo -e "\nSetting up composer ..."
+
+    pushd ./sysPass
+
+    if [ ! -e "./sysPass/composer.phar" ]; then
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+        php composer-setup.php
+        php -r "unlink('composer-setup.php');"
+    fi
+
+    php composer.phar self-update && php composer.phar install
+
+    popd
+}
+
 setup_locales() {
   if [ ! -e ".setup" ]; then
     LOCALE_GEN="/etc/locale.gen"
@@ -30,17 +61,27 @@ setup_locales() {
  fi
 }
 
-trap "echo 'Stopping Apache2 ...' && /usr/sbin/apachectl stop" HUP INT QUIT KILL TERM
+setup_apache () {
+    echo -e "Setting up xdebug variables ...\n"
+    sed -i 's/__XDEBUG_REMOTE_HOST__/'"$XDEBUG_REMOTE_HOST"'/' /etc/php/7.0/apache2/conf.d/20-xdebug.ini
+    sed -i 's/__XDEBUG_IDE_KEY__/'"$XDEBUG_IDE_KEY"'/' /etc/php/7.0/apache2/conf.d/20-xdebug.ini
+}
+
+echo "Starting with UID : ${SYSPASS_UID}"
+id ${SYSPASS_UID} > /dev/null 2>&1 || useradd --shell /bin/bash -u ${SYSPASS_UID} -o -c "" -m user
+export HOME=/home/user
 
 setup_locales
+setup_apache
+setup_app
+setup_composer
 
-echo -e "Setting xdebug variables ...\n"
-sed -i 's/__XDEBUG_REMOTE_HOST__/'"$XDEBUG_REMOTE_HOST"'/' /etc/php/7.0/apache2/conf.d/20-xdebug.ini
-sed -i 's/__XDEBUG_IDE_KEY__/'"$XDEBUG_IDE_KEY"'/' /etc/php/7.0/apache2/conf.d/20-xdebug.ini
+if [ "$1" == "apache" ]; then
+    # Apache gets grumpy about PID files pre-existing
+    rm -f ${APACHE_PID_FILE}
 
-[[ -e "/var/run/apache2/apache2.pid" ]] && rm -rf "/var/run/apache2/apache2.pid"
+    exec /usr/sbin/apache2ctl -DFOREGROUND
+fi
 
-echo -e "Starting Apache2 ...\n"
-/usr/sbin/apache2ctl -D FOREGROUND &
-
-wait $!
+echo -e "Starting $@ ...\n"
+exec gosu ${SYSPASS_UID} "$@"
