118782236a05f28d195dddb36eadc49975e6af9f
Rubén D <rdb@tier1.es>
2019-01-02 11:39:47 +0100

    [FIX] Fixed merged files

--- sysPass/entrypoint.sh
+++ sysPass/entrypoint.sh
@@ -5,23 +5,6 @@ COLOR_YELLOW='\033[0;33m'
 COLOR_RED='\033[0;31m'
 COLOR_GREEN='\033[0;32m'
 
-<<<<<<< HEAD
-setup_app () {
-  [[ ! -d "./sysPass" ]] && mkdir sysPass
-
-  if [ ! -e "./sysPass/index.php" ]; then
-    echo -e "${COLOR_YELLOW}setup_app: Unpacking sysPass '${SYSPASS_BRANCH}'${COLOR_NC}"
-
-    unzip ${SYSPASS_BRANCH}.zip
-    mv sysPass-${SYSPASS_BRANCH}/* sysPass
-    rm -rf sysPass-${SYSPASS_BRANCH}
-
-    echo -e "${COLOR_YELLOW}setup_app: Setting up permissions${COLOR_NC}"
-
-    chown ${APACHE_RUN_USER}:${SYSPASS_UID} -R sysPass/
-    chmod g+w -R sysPass/
-    chmod 750 sysPass/config sysPass/backup
-=======
 XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-"172.17.0.1"}
 XDEBUG_IDE_KEY=${XDEBUG_IDE_KEY:-"ide"}
 
@@ -58,7 +41,6 @@ setup_app () {
     ${SYSPASS_DIR}/composer.json \
     ${SYSPASS_DIR}/composer.lock \
     ${SYSPASS_DIR}/vendor
->>>>>>> 3.0
   fi
 }
 
@@ -78,11 +60,8 @@ setup_locales() {
     echo "ru_RU.UTF-8 UTF-8" >> $LOCALE_GEN
     echo "pl_PL.UTF-8 UTF-8" >> $LOCALE_GEN
     echo "nl_NL.UTF-8 UTF-8" >> $LOCALE_GEN
-<<<<<<< HEAD
-=======
     echo "pt_BR.UTF-8 UTF-8" >> $LOCALE_GEN
     echo "da.UTF-8 UTF-8" >> $LOCALE_GEN
->>>>>>> 3.0
 
     echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 
@@ -98,33 +77,6 @@ setup_locales() {
   fi
 }
 
-<<<<<<< HEAD
-echo -e "${COLOR_YELLOW}entrypoint: Starting with UID : ${SYSPASS_UID}${COLOR_NC}"
-id ${SYSPASS_UID} > /dev/null 2>&1 || useradd --shell /bin/bash -u ${SYSPASS_UID} -o -c "" -m user
-export HOME=/home/user
-
-setup_app
-
-case "$1" in
-  "apache")
-    setup_locales
-
-    SELF_IP_ADDRESS=$(grep $HOSTNAME /etc/hosts | cut -f1)
-
-    echo -e "${COLOR_GREEN}######"
-    echo -e "sysPass environment installed and configured. Please point your browser to http://${SELF_IP_ADDRESS} to start the installation"
-    echo -e "######${COLOR_NC}"
-    echo -e "${COLOR_YELLOW}entrypoint: Starting Apache${COLOR_NC}"
-
-    # Apache gets grumpy about PID files pre-existing
-    rm -f ${APACHE_PID_FILE}
-
-    exec /usr/sbin/apache2ctl -DFOREGROUND
-    ;;
-  *)
-    echo -e "${COLOR_YELLOW}entrypoint: Starting $@${COLOR_NC}"
-    exec "$@"
-=======
 run_composer () {
   pushd ${SYSPASS_DIR}
 
@@ -178,6 +130,5 @@ case "$1" in
   *)
     echo -e "${COLOR_YELLOW}entrypoint: Starting $@${COLOR_NC}"
     exec ${GOSU} "$@"
->>>>>>> 3.0
     ;;
 esac
