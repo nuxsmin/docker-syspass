9f422e37080c387fb805d0dc82ffc071271bf4cb
Rubén D <nuxsmin@gmail.com>
2019-08-18 23:19:27 +0200

    [MOD] Bump to v3.1

--- common/build.sh
+++ common/build.sh
@@ -2,8 +2,8 @@
 
 BUILDS=`find ../ -type d -name sysPass-*`
 BRANCH="master"
-VERSION="3.1.0-rc5"
-BUILD_NUMBER="19072001"
+VERSION="3.1.0"
+BUILD_NUMBER="19081801"
 
 build_env() {
   for BUILD in ${BUILDS}; do
