905ca606181432ff6ef9f4dfb32204fbbf508e33
Rubén D <nuxsmin@gmail.com>
2020-12-20 23:15:10 +0100

    [MOD] Drop PHP versions < 7.3 [MOD] Use sysPass 3.2 [MOD] Improved initialization [MOD] Added missing locales

--- sysPass-php7.3/common_fn.sh
+++ sysPass-php7.3/common_fn.sh
@@ -1,20 +1,55 @@
-#!/bin/bash
+: ${XDEBUG_REMOTE_HOST:="172.17.0.1"}
+: ${XDEBUG_IDE_KEY:="ide"}
+: ${SYSPASS_DEV:=0}
+: ${PHP_XDEBUG_FILE:="/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"}
+: ${SYSPASS_LOCALES:="es_ES en_US en_GB de_DE ca_ES fr_FR ru_RU pl_PL nl_NL pt_BR da_DK it_IT fo_FO ja_JP"}
+: ${COMPOSER_EXTENSIONS:=}
+: ${DEBUG:=0}
+
+if [ ${DEBUG} -eq 1 ]; then
+  set -x
+fi
+
+COMPOSER_OPTIONS="--working-dir ${SYSPASS_DIR} --classmap-authoritative"
+GOSU="gosu ${SYSPASS_UID}"
 
 COLOR_NC='\033[0m'
 COLOR_YELLOW='\033[0;33m'
 COLOR_RED='\033[0;31m'
 COLOR_GREEN='\033[0;32m'
 
-XDEBUG_REMOTE_HOST=${XDEBUG_REMOTE_HOST:-"172.17.0.1"}
-XDEBUG_IDE_KEY=${XDEBUG_IDE_KEY:-"ide"}
-
-COMPOSER_OPTIONS="--working-dir ${SYSPASS_DIR} --classmap-authoritative"
-
-GOSU="gosu ${SYSPASS_UID}"
-
-if [ -e /usr/local/sbin/init-functions ]; then
-  . /usr/local/sbin/init-functions
+if [ ${SYSPASS_DEV} -eq 1 ]; then
+  COMPOSER_OPTIONS="--working-dir ${SYSPASS_DIR} --optimize-autoloader --dev"
 fi
+
+setup_apache () {
+  if [ ${SYSPASS_DEV} -eq 0 ]; then
+    return 0
+  fi
+
+  echo -e "${COLOR_YELLOW}setup_apache: Setting up xdebug variables${COLOR_NC}"
+
+  sed -i 's/__XDEBUG_REMOTE_HOST__/'"$XDEBUG_REMOTE_HOST"'/;
+  s/__XDEBUG_IDE_KEY__/'"$XDEBUG_IDE_KEY"'/' \
+  ${PHP_XDEBUG_FILE}
+}
+
+run_apache () {
+  : ${PHP_INI_DIR:=}
+
+  if [ -z "${PHP_INI_DIR}" ]; then
+    echo -e "${COLOR_YELLOW}run_apache: Starting Apache${COLOR_NC}"
+
+    # Apache gets grumpy about PID files pre-existing
+    rm -f ${APACHE_PID_FILE}
+
+    exec /usr/sbin/apache2ctl -DFOREGROUND
+  else
+    echo -e "${COLOR_YELLOW}run_apache: Starting Apache (PHP)${COLOR_NC}"
+
+    apache2-foreground
+  fi
+}
 
 setup_app () {
   if [ -e "${SYSPASS_DIR}/index.php" ]; then
@@ -45,19 +80,10 @@ setup_locales() {
     echo -e "${COLOR_YELLOW}setup_locales: Setting up locales${COLOR_NC}"
 
     echo -e "\n### sysPass locales" >> $LOCALE_GEN
-    echo "es_ES.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "en_US.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "en_GB.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "de_DE.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "ca_ES.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "fr_FR.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "ru_RU.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "pl_PL.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "nl_NL.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "pt_BR.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "da.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "it_IT.UTF-8 UTF-8" >> $LOCALE_GEN
-    echo "fo.UTF-8 UTF-8" >> $LOCALE_GEN
+
+    for LOCALE in ${SYSPASS_LOCALES}; do
+      echo "${LOCALE}.UTF-8 UTF-8" >> $LOCALE_GEN
+    done
 
     echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 
@@ -94,37 +120,3 @@ setup_composer_extensions () {
     run_composer require ${COMPOSER_EXTENSIONS} --update-no-dev
   fi
 }
-
-echo -e "${COLOR_YELLOW}entrypoint: Starting with UID : ${SYSPASS_UID}${COLOR_NC}"
-id ${SYSPASS_UID} > /dev/null 2>&1 || useradd --shell /bin/bash -u ${SYSPASS_UID} -o -c "" -m user
-export HOME=${SYSPASS_DIR}
-
-setup_app
-
-case "$1" in
-  "apache")
-    setup_composer_extensions
-    setup_locales
-    setup_apache
-
-    SELF_IP_ADDRESS=$(grep $HOSTNAME /etc/hosts | cut -f1)
-
-    echo -e "${COLOR_GREEN}######"
-    echo -e "sysPass environment installed and configured. Please point your browser to https://${SELF_IP_ADDRESS} to start the installation"
-    echo -e "######${COLOR_NC}"
-    echo -e "${COLOR_YELLOW}entrypoint: Starting Apache${COLOR_NC}"
-
-    run_apache
-    ;;
-  "update")
-    run_composer update
-    ;;
-  "composer")
-    shift
-    run_composer "$@"
-    ;;
-  *)
-    echo -e "${COLOR_YELLOW}entrypoint: Starting $@${COLOR_NC}"
-    exec ${GOSU} "$@"
-    ;;
-esac
